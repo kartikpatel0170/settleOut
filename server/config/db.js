e82400d3152ccb389c962596cda413f11b743f8c
Kartik Patel <kartikpatel0170@gmail.com>
2023-02-16 20:53:32 -0600

    connected db

---
+++ server/config/db.js
@@ -0,0 +1,16 @@
+/* Database Init */
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("MongoDB connection is successful");
+});
+
+connection.on("error", (error) => {
+  console.log("Error in MongoDB connection", error);
+});
+
+module.exports = mongoose;
