ded22a7b22e188102b2568af918350a1cc7c1aa4
Kartik Patel <kartikpatel0170@gmail.com>
2023-02-21 23:54:34 -0600

    fixed configuration for api testing

--- server.js
+++ server.js
@@ -3,15 +3,37 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const _ = require("lodash");
 const http = require("http");
+const session = require("express-session");
+let logger = require("morgan");
+const passport = require("passport");
+let cookieParser = require("cookie-parser");
+global.__basedir = __dirname;
+const { JWT } = require("./config/authConstant");
 const app = express();
 const server = http.createServer(app);
+const path=require("path");
+
+// Middleware for all the requests
+app.use(session({ secret: JWT.SECRET, saveUninitialized: true, resave: true }));
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use(cors());
 
 
+// Authentication middleware
+app.use(passport.initialize());
+app.use(passport.session());
+require("./config/passport")(passport);
+
+
 // connecting to the database
 require("./config/db");
+app.use(require("./routes/index"));
 server.listen(process.env.PORT, () => {
     console.log(`your application is running on ${process.env.PORT}`);
   });
