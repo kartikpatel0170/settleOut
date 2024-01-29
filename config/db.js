c29fe691a48956ebd6686fc523dfcbb03f437716
kpatel0170 <kartikpatel0170@gmail.com>
2024-01-28 22:55:34 -0600

    feat: update mongoDB config

--- config/db.js
+++ config/db.js
@@ -1,19 +1,36 @@
-/*
- * Database connection file.
- */
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const logger = require("./logger");
 
-const dConnection = process.env.MONGO_URL
+const mongoURL = process.env.MONGO_URL;
 
-mongoose.connect(dConnection, { useNewUrlParser: true, useUnifiedTopology: true })
-var db = mongoose.connection
+mongoose.connect(mongoURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
-db.once("open", () => {
-    console.log("Connection Succeed")
-})
+const db = mongoose.connection;
 
-db.on("error", () => {
-    console.error("Error in Connect Mongo")
-})
+db.on("connected", () => {
+  logger.info(`Connected to MongoDB`);
+});
 
-module.exports = mongoose
+db.on("error", (err) => {
+  logger.error(`Error in MongoDB connection: ${err}`);
+});
+
+db.on("disconnected", () => {
+  logger.error("MongoDB disconnected");
+});
+
+process.on("SIGINT", async () => {
+  try {
+    await mongoose.connection.close();
+    logger.info("MongoDB connection closed through app termination");
+    process.exit(0);
+  } catch (err) {
+    logger.error(`Error closing MongoDB connection: ${err}`);
+    process.exit(1);
+  }
+});
+
+module.exports = mongoose;
